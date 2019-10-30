9e79b4c8f15d0a6503bc36c1b43e2b03967ebaea
Administrator <xfcomwork@163.com>
2019-10-30 17:38:26 +0800

    搭建项目

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,118 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
-  </div>
+  <el-row :gutter="20">
+    <el-col :span="20" :offset="2">
+      <div class="grid-content bg-purple">
+        <avue-crud
+          :data="data"
+          :option="option"
+          :page="page"
+          @size-change="sizeChange"
+          @current-change="currentChange"
+        ></avue-crud>
+        <br>
+    </div>
+    </el-col>
+  </el-row>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    data() {
+      return {
+        page: {
+          pageSizes: [1, 5, 10, 15],
+          currentPage: 1,
+          total: 0,
+          pageSize: 2
+        },
+        data: [
+          {
+            name: '张三',
+            sex: '男'
+          },
+          {
+            name: '李四',
+            sex: '女'
+          }
+        ],
+        option: {
+          align: 'center',
+          menuAlign: 'center',
+          column: [
+            {
+              label: '姓名',
+              prop: 'name'
+            },
+            {
+              label: '性别',
+              prop: 'sex'
+            }
+          ]
+        }
+      }
+    },
+    created() {
+      this.getList()
+    },
+    methods: {
+      getList() {
+        this.page.total = 10
+        if (this.page.currentPage === 1) {
+          this.data = [
+            {
+              name: '张三',
+              sex: '男'
+            }
+          ]
+        } else if (this.page.currentPage == 2) {
+          this.data = [
+            {
+              name: '李四',
+              sex: '女'
+            }
+          ]
+        }
+      },
+      sizeChange(val) {
+        this.page.currentPage = 1
+        this.page.pageSize = val
+        this.getList()
+        this.$message.success('行数' + val)
+      },
+      currentChange(val) {
+        this.page.currentPage = val
+        this.getList()
+        this.$message.success('页码' + val)
+      }
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .el-row {
+    margin-bottom: 20px;
+  }
+  el-row:last-child {
+     margin-bottom: 0;
+   }
+  .el-col {
+    border-radius: 4px;
+  }
+  .bg-purple-dark {
+    background: #99a9bf;
+  }
+  .bg-purple {
+    background: #d3dce6;
+  }
+  .bg-purple-light {
+    background: #e5e9f2;
+  }
+  .grid-content {
+    border-radius: 4px;
+    min-height: 36px;
+  }
+  .row-bg {
+    padding: 10px 0;
+    background-color: #f9fafc;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,9 +2,13 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import Avue from '@smallwei/avue';
+import '@smallwei/avue/lib/index.css';
 
-Vue.config.productionTip = false
-
+Vue.use(Avue);
+Vue.use(ElementUI)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
